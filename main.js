3f818b4f1fb9e3bb72687c260100ce6ff4da171a
William Kim <jesung1221@hotmail.com>
2016-07-13 01:29:35 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,59 +1,29 @@
-//meteor add accounts-password <-a package which manages user collection (Ex: meteor.users)
-//meteor add accounts-ui <-package which helps build ui for login
-//meteor remove autopublish <- so that users can't access entire database.
-//meteor remove insecure <- prevent users from inserting data into collection
+PlayerList = new Mongo.Collection('players');
+//meteor.publish determines what data could be accessible
+Meteor.publish('thePlayers',function(){
+	var currentUserId = this.userId; //
+	return PlayerList.find({createdBy: currentUserId});
+})
 
-PlayerList = new Mongo.Collection('players'); //reference variable PlayerList points to a Collection called players
-
-Meteor.subscribe('thePlayers'); //subscribe retrieves accessible data
-
-Template.leaderboard.helpers({ //helper functions go inside templates
-	'player': function(){
+Meteor.methods({
+	'sendLogMessage': function(){
+		console.log("Hello World");
+	},
+	'insertPlayerData': function(playerNameVar){
 		var currentUserId = Meteor.userId();
-		return PlayerList.find({},{sort: {score:-1, name:1}});
+		PlayerList.insert({
+			name: playerNameVar,
+			score:0,
+			createdBy:currentUserId
+		});
 	},
-		 //1st argument: returns all documents. 2nd argument, sort scores in descending, names in ascending order.
-	'selectedClass': function(){
-		var playerId = this._id;
-		var selectedPlayer = Session.get('selectedPlayer');
-		if(playerId == selectedPlayer){
-			return "selected";
-		}
+	'removePlayerData': function(selectedPlayer){
+		var currentUserId = Meteor.userId();
+		PlayerList.remove({_id: selectedPlayer, createdBy: currentUserId});
 	},
-	'showSelectedPlayer': function(){
-		var selectedPlayer = Session.get('selectedPlayer');
-		return PlayerList.findOne(selectedPlayer);
+	'modifyPlayerScore': function(selectedPlayer,scoreValue){
+		var currentUserId = Meteo.userId();
+		PlayerList.update({_id: selectedPlayer, createdBy: currentUserId}, {$inc: {score:scoreValue}}); //mongo operator $set (updates without deleting the original document)
 	}
-});
 
-Template.leaderboard.events({
-	'click .player': function(){ //this player class refrers to <li class="player"> in html 
-		var playerId = this._id; //refers to the primary key of the document
-		Session.set('selectedPlayer', playerId); //store playerId on session called selectedPlayer
-	},
-	'click .increment': function(){
-		var selectedPlayer = Session.get('selectedPlayer');
-		Meteor.call('modifyPlayerScore',selectedPlayer, 5);
-
-		
-	},
-	'click .decrement': function(){
-		var selectedPlayer = Session.get('selectedPlayer');
-		Meteor.call('modifyPlayerScore',selectedPlayer, -5);
-	}
-});
-
-Template.addPlayerForm.events({
-	'submit form': function(event){
-	event.preventDefault();//prevents default behavior of submmiting forms.(Prevents refreshing of the page)
-	var playerNameVar = event.target.playerName.value;
-	var currentUserId = Meteor.userId(); //returns unique ID of the currently logged in user
-	
-	Meteor.call('insertPlayerData',playerNameVar);
-	},
-
-	'click .remove': function(){
-		var selectedPlayer = Session.get('selectedPlayer');
-		Meteor.call('removePlayerData', selectedPlayer);
-	}
 });
